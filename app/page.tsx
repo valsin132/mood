cb569cff2237d75779625bb34f151c193eb1e3e2
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-02-11 23:43:07 +0200

    added Clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
-        <h1 className="text-6xl mb-4">The best Journal app</h1>
+        <h1 className="text-6xl mb-4">The best Journal App.</h1>
         <p className="text-2xl text-white/60 mb-4">
           This is the best app for tracking your mood through out your life. All
           you have to do is be honest.
