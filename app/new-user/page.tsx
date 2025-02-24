12c461810720fbae0441dc8934a67413361aacd7
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-02-24 18:46:26 +0200

    added createNewUser

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,24 @@
+import { prisma } from '@/utils/db'
+import { auth, currentUser } from '@clerk/nextjs/dist/types/server'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  })
+
+  if (!match) {
+    const user = await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.email,
+      },
+    })
+  }
+}
+
 const NewUser = () => {
   return <div>hi</div>
 }
