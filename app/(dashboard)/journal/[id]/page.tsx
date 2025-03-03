ae5cab96675c813a842409a8d5257681783265ba
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-03-03 20:56:54 +0200

    added editor

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,5 +1,28 @@
-const EntryPage = ({ params }) => {
-  return <div>{params.id}</div>
+import Editor from '@/components/Editor'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntry = async (id) => {
+  const user = await getUserByClerkId()
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  })
+
+  return entry
+}
+
+const EntryPage = async ({ params }) => {
+  const entry = await getEntry(params.id)
+  return (
+    <div className="h-full w-full">
+      <Editor entry={entry} />
+    </div>
+  )
 }
 
 export default EntryPage
---
+++ components/Editor.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useState } from 'react'
+
+const Editor = ({ entry }) => {
+  const [value, setValue] = useState(entry.content)
+
+  return (
+    <div className="w-full h-full">
+      <textarea
+        className="w-full h-full p-8 text-xl"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  )
+}
+
+export default Editor
