29f5627c311fb444d0db2d10250f8e5faaa35263
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-04-29 23:14:48 +0300

    new-user checked version

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,36 +1,3 @@
-import { prisma } from '@/utils/db'
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
-
-const createNewUser = async () => {
-  const user = await currentUser()
-  console.log(user)
-
-  const match = await prisma.user.findUnique({
-    where: {
-      clerkId: user.id as string,
-    },
-  })
-
-  if (!match) {
-    await prisma.user.create({
-      data: {
-        clerkId: user.id,
-        email: user?.emailAddresses[0].emailAddress,
-      },
-    })
-  }
-
-  redirect('/journal')
-}
-
-const NewUser = async () => {
-  await createNewUser()
-  return <div>...loading</div>
-}
-
-export default NewUser
-
 // import { prisma } from '@/utils/db'
 // import { currentUser } from '@clerk/nextjs/server'
 // import { redirect } from 'next/navigation'
@@ -39,28 +6,17 @@ export default NewUser
 //   const user = await currentUser()
 //   console.log(user)
 
-//   if (!user) return
-
-//   const { id: clerkId, emailAddresses } = user
-//   const email = emailAddresses[0]?.emailAddress
-
-//   if (!email) return
-
-//   // 🔎 Check if user exists by clerkId or email
-//   const existingUser = await prisma.user.findFirst({
+//   const match = await prisma.user.findUnique({
 //     where: {
-//       OR: [
-//         { clerkId },
-//         { email },
-//       ],
+//       clerkId: user.id as string,
 //     },
 //   })
 
-//   if (!existingUser) {
+//   if (!match) {
 //     await prisma.user.create({
 //       data: {
-//         clerkId,
-//         email,
+//         clerkId: user.id,
+//         email: user?.emailAddresses[0].emailAddress,
 //       },
 //     })
 //   }
@@ -74,3 +30,47 @@ export default NewUser
 // }
 
 // export default NewUser
+
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  console.log(user)
+
+  if (!user) return
+
+  const { id: clerkId, emailAddresses } = user
+  const email = emailAddresses[0]?.emailAddress
+
+  if (!email) return
+
+  // 🔎 Check if user exists by clerkId or email
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [
+        { clerkId },
+        { email },
+      ],
+    },
+  })
+
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        clerkId,
+        email,
+      },
+    })
+  }
+
+  redirect('/journal')
+}
+
+const NewUser = async () => {
+  await createNewUser()
+  return <div>...loading</div>
+}
+
+export default NewUser
